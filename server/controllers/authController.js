2c99daa981ecfbce625cf71809b4743c65205ab4
ajith kumar v <vajithkumar300@gmail.com>
2025-11-07 20:07:19 +0530

    try to fix in production while refrech loss the jwt tokken

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.COOKIE_SECURE === 'true',
-      sameSite: 'lax',
+      sameSite: process.env.COOKIE_SECURE === 'true' ? 'none' : 'lax',
       maxAge: 1000 * 60 * 60 * 24 * 7
     });
 
